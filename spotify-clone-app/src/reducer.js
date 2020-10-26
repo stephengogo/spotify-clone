a3ffd263a79da5d6a9883456607d549bfe1d3b9f
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-26 11:27:37 -0700

    playlist from spotify
    import your playlist from real spotify

--- spotify-clone-app/src/reducer.js
+++ spotify-clone-app/src/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
     playing: false, 
     item: null,
     // make null after developing
-    token: 'BQDDTgCK2o2gi1N8XL8DaGXPJF4FwGsFNL-bS8IDUSVpnqc8RMUWqHTeKw4PxlEkTuyEk23h6d-srrSHLB-PpbX2tszglrhMv5lgYOhV3iJp3DBJqWdmeaSDMoYS5FItB0g_mo9VwStL47J9KE5heS7GknPyggZ5BuH46pm1GxWcV6IY'
+    // token: 'BQDDTgCK2o2gi1N8XL8DaGXPJF4FwGsFNL-bS8IDUSVpnqc8RMUWqHTeKw4PxlEkTuyEk23h6d-srrSHLB-PpbX2tszglrhMv5lgYOhV3iJp3DBJqWdmeaSDMoYS5FItB0g_mo9VwStL47J9KE5heS7GknPyggZ5BuH46pm1GxWcV6IY'
 };
 
 const reducer = (state, action) => {
@@ -16,12 +16,17 @@ const reducer = (state, action) => {
         case 'SET_USER': 
             return {
                 ...state,
-                user: action.user 
+                user: action.user ,
             }
         case 'SET_TOKEN':
             return {
                 ...state,
-                token: action.token
+                token: action.token,
+            }
+        case 'SET_PLAYLISTS': 
+            return {
+                ...state, 
+                playlists: action.playlists,
             }
         default: 
             return state;
